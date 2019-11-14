503436e07d20119a4287db04b583df695210b333
neverdie <altiore@mail.ru>
2019-11-14 17:59:40 +0300

    Add Complaint Model.

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -26,3 +27,12 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"От {self.user} на {self.flat}"
